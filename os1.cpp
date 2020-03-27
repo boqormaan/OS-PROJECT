179cde3dea848607a45e4c377139e80fa49b3179
boqormaan <56436409+boqormaan@users.noreply.github.com>
2020-03-27 18:41:37 +0530

    Add files via upload

---
+++ os1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m,i,h,g;
+	n=5;
+	m=3;
+	int alloc[5][3]={{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+	int max[5][3]={{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
+	int avail[3]={3,2,2};
+	int f[n],ans[n],ind=0;
+	for(g=0;g<n;g++)
+	{
+		f[g]=0;
+	}
+}
