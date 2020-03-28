f81fabd482b0ad53dacbd7a7908b349e35bd0ef5
boqormaan <56436409+boqormaan@users.noreply.github.com>
2020-03-28 18:31:13 +0530

    Add files via upload

---
+++ complete code.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m,i,h,g;
+	n=5;
+	m=3;
+	int alloc[5][3]={{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+	int max[5][3]={{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
+	int avail[3]={3,2,2};
+	int f[n],ans[n],in=0;
+	for(g=0;g<n;g++)
+	{
+		f[g]=0;
+	}
+	int need[n][m];
+	for(i=0;i<n;i++)
+	{
+		for(h=0;h<m;h++)
+		need[i][h]=max[i][h]-alloc[i][h];
+	}
+	int z=0;
+	for(g=0;g<5;g++)
+	{
+		for(i=0;i<n;i++)
+		{
+			if(f[i]==0)
+			{
+				int flag=0;
+				for(h=0;h<m;h++)
+				{
+					if(need[i][h]>avail[h])
+					{
+						flag=1;
+						break;
+					}
+				}
+				if(flag==0)
+				{
+					ans[in++]=i;
+					for(z=0;z<m;z++)
+					avail[z]+=alloc[i][z];
+					f[i]=1;
+				}
+			}
+			
+		}
+	}
+	cout<<"following is the safe sequence"<<endl;
+	for(i=0;i<n-1;i++)
+	   cout<<"p"<<ans[i]<<"--";
+	cout<<"p"<<ans[n-1]<<endl;
+	return(0);
+}
+
